44bfc65ae18fad40000eda82865862116ab9cd4d
xuejia.wang <wxjwangxuejia@outlook.com>
2020-08-01 17:34:04 +0800

    feat: modify the ui to adapt resolution
    Signed-off-by: xuejia.wang <wxjwangxuejia@outlook.com>

--- Assets/Scripts/UI/Component/ComponentUIManager.cs
+++ Assets/Scripts/UI/Component/ComponentUIManager.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class ComponentUIManager : MonobehaviorSingleton<ComponentUIManager>
 {
     private TransformUIModel transformModel = null;
+    private Transform componentGroup = null;
     protected override void Init()
     {
         base.Init();
+        componentGroup = transform.Find("ComponentGroup");
         InteractiveGameObjectCollection.Instance.OnHoldingInteractiveGOUpdated += OnSelectedGoUpdated;
 
         transformModel = GameResourceManager.Instance.CreateEntityController<TransformUIModel>("component_transform").
                             model as TransformUIModel;
-        transformModel.view.transform.SetParent(transform, false);
+        transformModel.view.transform.SetParent(componentGroup, false);
 
         transformModel.active = false;
     }
