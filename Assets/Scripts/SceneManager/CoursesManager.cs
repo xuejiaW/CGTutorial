c1726dea9e50e586d014b59821a019dc7856f39c
xuejia.wang <wxjwangxuejia@outlook.com>
2020-08-06 21:10:06 +0800

    feat: add CourseManager
    Signed-off-by: xuejia.wang <wxjwangxuejia@outlook.com>

---
+++ Assets/Scripts/SceneManager/CoursesManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//TODO: make async load scene
+public class CoursesManager : MonoBehaviour
+{
+    public void OnClickCourseTriangle()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
