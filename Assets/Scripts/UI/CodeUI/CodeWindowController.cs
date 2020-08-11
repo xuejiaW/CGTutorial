97427e714ecc162205cf856451a87aa0c7b9b2c4
xuejia.wang <wxjwangxuejia@outlook.com>
2020-08-11 14:50:34 +0800

    feat: add code ui tab window
    Signed-off-by: xuejia.wang <wxjwangxuejia@outlook.com>

---
+++ Assets/Scripts/UI/CodeUI/CodeWindowController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CodeWindowController : MonoBehaviour
+{
+    public CodeWindowManager manager = null;
+    private float aniDuration = 0.5f;
+
+    [System.NonSerialized]
+    public int windowIndex = -1;
+    [System.NonSerialized]
+    public float windowWidth = 0.0f;
+
+    public void Select(int index)
+    {
+        if (index == windowIndex)
+        {
+            gameObject.SetActive(true);
+            manager.duringAni = true;
+            transform.SetPositionX(transform.localPosition.x < 0 ? -windowWidth : windowWidth);
+        }
+
+        float targetX = index == windowIndex ? 0 : (windowIndex < index ? -windowWidth : windowWidth);
+        transform.DOLocalMoveX(targetX, aniDuration).OnComplete(() =>
+        {
+            manager.duringAni = false;
+            gameObject.SetActive(windowIndex == manager.currentWindowIndex);
+        });
+    }
+}
---
+++ Assets/Scripts/UI/CodeUI/CodeWindowManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeWindowManager : MonoBehaviour
+{
+    private List<CodeWindowController> windowControllers = null;
+    public int currentWindowIndex = 0;
+    [System.NonSerialized]
+    public bool duringAni = false;
+
+    private void Start()
+    {
+
+        windowControllers = new List<CodeWindowController>(transform.GetComponentsInChildren<CodeWindowController>());
+
+        float windowWidth = GetComponent<RectTransform>().rect.width;
+        for (int i = 0; i != windowControllers.Count; ++i)
+        {
+            windowControllers[i].manager = this;
+            windowControllers[i].windowIndex = i;
+            windowControllers[i].gameObject.SetActive(i == 0);
+            windowControllers[i].windowWidth = windowWidth;
+
+            Transform windowTrans = windowControllers[i].transform;
+            Vector3 localPos = windowTrans.localPosition;
+            windowTrans.localPosition = localPos.SetX(i * windowWidth);
+        }
+    }
+
+    public void OnClickWindowTab(int index)
+    {
+        if (currentWindowIndex == index || duringAni)
+            return;
+
+        currentWindowIndex = index;
+        windowControllers.ForEach(window => window.Select(index));
+    }
+}
