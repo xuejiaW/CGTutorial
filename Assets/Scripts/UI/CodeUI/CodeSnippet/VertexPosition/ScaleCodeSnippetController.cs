5a7b6b99d3a3d64f432ab589c0f8d67d5a4cf48d
xuejia.wang <wxjwangxuejia@outlook.com>
2020-08-30 19:42:52 +0800

    feat: add rotation and scale snippet
    Signed-off-by: xuejia.wang <wxjwangxuejia@outlook.com>

---
+++ Assets/Scripts/UI/CodeUI/CodeSnippet/VertexPosition/ScaleCodeSnippetController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleCodeSnippetController : TransformCodeSnippetController
+{
+    public override UpdateModelPropertyBase GetModelUpdater()
+    {
+        return new LocalScaleModelUpdater();
+    }
+}
---
+++ Assets/Scripts/UI/CodeUI/CodeSnippet/VertexPosition/ScaleCodeSnippetView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleCodeSnippetView : TransformCodeSnippetView
+{
+    public override void Switch(bool on)
+    {
+        base.Switch(on);
+
+        if (on)
+            viewUpdater.UpdateView(targetGO.localScale);
+        else
+            viewUpdater.UpdateView(model.targetGameObject.localScale);
+    }
+
+
+    public override UpdateViewBase GetViewUpdater()
+    {
+        return new LocalScaleViewUpdater();
+    }
+}
